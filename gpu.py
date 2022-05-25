247853d829cbd2207d0b48ea84911b1c2fd2374e
AvinashWaghmare <49596012+AvinashWaghmare@users.noreply.github.com>
2022-05-25 22:26:09 +0530

    Add files via upload

---
+++ gpu.py
@@ -0,0 +1,3 @@
+import tensorflow as tf
+print(tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None))
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
