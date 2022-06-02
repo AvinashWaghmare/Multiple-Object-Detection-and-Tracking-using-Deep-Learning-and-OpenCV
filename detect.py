30c2aae1a65306f807e2c8ad8fbf8bf3db6277ce
AvinashWaghmare <49596012+AvinashWaghmare@users.noreply.github.com>
2022-06-02 14:48:08 +0530

    Updated detect.py

--- detect.py
+++ detect.py
@@ -217,29 +217,6 @@ def tasks():
     return render_template('index.html')
 
 
-@app.route('/detect', methods=['POST', 'GET'])
-def detect():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
-    else:
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        subprocess.call('dir', shell=True)
-        subprocess.call(["python", "detect.py", os.path.join(app.config['UPLOAD_FOLDER'], filename)], shell=True)
-        # print('upload_video filename: ' + filename)
-        flash('Video successfully uploaded and displayed below')
-        return render_template('index.html', filename=filename)
-
-
-@app.route('/display/<filename>')
-def display_video(filename):
-    # print('display_video filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 
 if __name__ == '__main__':
